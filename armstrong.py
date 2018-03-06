ce656a98665a6bd79b08145785a41ffe13d10385
saran1211 <36408495+saran1211@users.noreply.github.com>
2018-03-06 17:16:00 +0530

    armstrong
    check whether the num is armstrong or not

---
+++ armstrong.py
@@ -0,0 +1,12 @@
+num = 125
+order = len(str(num))
+sum = 0
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** order
+   temp //= 10
+if num == sum:
+   print(num,"is an Armstrong num")
+else:
+   print(num,"is not an Armstrong num")
